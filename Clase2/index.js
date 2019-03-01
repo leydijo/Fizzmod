974464fe6050b4cc18a04ccff6c2969ae9cfb334
leydijo <leydilenis85@gmail.com>
2019-02-28 21:59:32 -0300

    apuntesjs

--- Clase2/index.js
+++ Clase2/index.js
@@ -6,8 +6,10 @@
     y puede variar por lugar de ejecucion o desicion del programador.
     un console.dir  console.dir(persona)
     */
+
 let a ={}
-a.toSting()
+   a.toSting()
+
 
 function  persona(){
    
@@ -15,3 +17,76 @@ function  persona(){
     
 }
 persona()
+
+function ctx(a,b){
+    console.log("parametros:", a,b)
+    console.log("contexto:", this)
+    console.log("**************************", )
+}
+ctx(1,2)
+
+//call y apply: ejecutan la funcion
+ctx.call({ctx:"call"}, 10,20)
+ctx.apply({ctx:"apply"},[100,200])
+
+//BIND : retorna la definicion de la funcion
+ctx.bind({ctx:"bind"},100,200)
+/* New : ejecuta la funcion que tiene al lado reasignandle el contexto con un onjetp vacio. al finalizar
+la ejecucion de la funcion , retorna ese objeto
+*/
+new ctx // si l ejecitas asi , te muestra undefined,undefined,undefined y un obj vacio
+
+let a ={}
+ctx.call(a)
+return a
+
+
+/*formas de ejecutar una function
+ctx(1,2) //1 2window{}
+ctx.call({ctx:"call"}, 10,20)
+*/
+
+function foo(a,b){
+    
+    console.log(a,b)
+}
+document.addEventListener("click",foo.bind(null,1,2))
+
+let protipo_uno ={
+    saludo:function(){}
+    constructor: function fizzmod(){}
+}
+let instancia_uno = Object.create(protipo_uno)
+
+
+
+
+// closure es un espacio reservado que esta defina dentro de otra funcion,es un spoce temporal
+
+function externa (x){
+    console.log(x)
+
+    return function interna(y){
+        console.log(y+x)
+    }
+}
+let retorno = externa(10)
+retorno(20)
+
+function persona(){
+    //atributo "privado" de clase que le pertenece al closure de la funcion que la use
+    let nombre =  "leydi"
+
+    // metodo publico de instancia que guarda como referencia el atributo "privado" del cloure
+    this.getnombre = function(){
+        console.log(nombre)
+    }
+}
+
+new Persona
+
+ /*funciones IIfe=immediatly Invoked function
+     Heap: declaracion de variables y funciones
+     stack: asigancion de valores y ejecucio en ordem
+ */
+
